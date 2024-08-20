ad1b86a37ec0bf4f939df0df84d844824bb3bd1a
Aarushi gupta <115031972+aaruhsigupta@users.noreply.github.com>
2024-08-21 00:45:31 +0530

    Add files via upload

---
+++ src/components/Projects/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectStyle'
+import ProjectCards from '../Cards/ProjectCard'
+import { projects } from '../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'frontend project' ?
+            <ToggleButton active value="frontend project" onClick={() => setToggle('frontend project')}>Frontend Project</ToggleButton>
+            :
+            <ToggleButton value="frontend project" onClick={() => setToggle('frontend project')}>Frontend Project</ToggleButton>
+          }
+          <Divider />
+          
+        
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
